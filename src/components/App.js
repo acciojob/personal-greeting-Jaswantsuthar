71300977a8119829e9ca06ac70c6753dfd290e9a
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-08-24 11:11:34 +0530

    js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[event,setEvent]=useState("")
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name</p>
+        <input type="text" onChange={(e)=>{
+          setEvent("hello "+e.target.value+"!")
+        }}/>
+        <div>{event}</div>
     </div>
   )
 }
